5c3e32c8736c919c86ddab592bc829172f254290
Owen Sullivan <opsullivan85@gmail.com>
2020-05-18 17:15:55 -0400

    Updated Buildplate Size

--- Marlin-1.1.9.1/Marlin/Configuration.h
+++ Marlin-1.1.9.1/Marlin/Configuration.h
@@ -877,7 +877,7 @@
 
 // The size of the print bed
 #define X_BED_SIZE 300
-#define Y_BED_SIZE 300
+#define Y_BED_SIZE 190
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
@@ -885,7 +885,7 @@
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 240
+#define Z_MAX_POS 350
 
 /**
  * Software Endstops
